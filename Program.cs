5dcaeac44c0f4438c651df24fb854048cb767653
unknown <Andrew M>
2022-12-04 20:58:07 +0300

    Give up for today

--- Program.cs
+++ Program.cs
@@ -18,7 +18,11 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
-
+// int Count(int a){
+//     for (int a > 100, )
+//     if (a < 100)
+//         Console.WriteLine("Третьей цифры нет");
+// }
 
 
 
@@ -28,14 +32,14 @@
 // 1 -> нет
 
 
-void Day(int num){
-    if (num < 6)
-        Console.WriteLine("Будний день");
-    else if (num > 7)
-        Console.WriteLine("Попробуйте еще раз ввести целое число от 1 до 7");
-    else
-        Console.WriteLine("Выходной день");
-}
-Console.WriteLine("Введите целое число от 1 до 7");
-int num = Convert.ToInt32(Console.ReadLine());
-Day (num);
+// void Day(int num){
+//     if (num < 6)
+//         Console.WriteLine("Будний день");
+//     else if (num > 7)
+//         Console.WriteLine("Попробуйте еще раз ввести целое число от 1 до 7");
+//     else
+//         Console.WriteLine("Выходной день");
+// }
+// Console.WriteLine("Введите целое число от 1 до 7");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Day (num);
